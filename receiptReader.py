bb72bf90406d0e0ec4e930474376c0898932e340
JJ80901 <j.j.cavazos80@gmail.com>
2024-07-26 23:53:42 -0500

    Made receipt-reader.py cleaner and added comments

--- receiptReader.py
+++ receiptReader.py
@@ -13,24 +13,24 @@ url = "https://receipt.365retailmarkets.com/receipt/view?transid=VSH603038-23013
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
-# Parse the content
+# Parse the content into list
 Data = []
 for row in soup.find_all('td'):
     data = row.text.strip()
     if 'Thank' not in data and 'Welcome' not in data and 'Copyright' not in data and 'Payments' not in data:
         Data.append(data)
 
-#print(Data)
-
+#Establish important values
 Location = Data[0]
 TimeNDate = Data[1]
-PaymentType = ''
 Items = []
 Costs = []
-Total = 0
-TotalTax = 0
 Subtotal = 0
+TotalTax = 0
+Total = 0
+PaymentType = ''
 
+#Sort through Data list and add values based off of context
 for i in range(len(Data)):
     if (Data[i] == 'Amount'):
         PaymentType = Data[i + 1]
@@ -41,6 +41,9 @@ for i in range(len(Data)):
     if (Data[i] == 'Total'):
         Total = float(Data[i + 1][1:])
 
+#Start adding items and their respective cost to their lists
+#Starting at index 2 since indexes 0 and 1 are the location and time/date
+#This list will iterate until 'Subtotal' is found, leaving room to digest smaller and larger receipts
 j = 2
 while (j < Data.index('Subtotal')):
     if (j % 2 == 0):
@@ -49,7 +52,7 @@ while (j < Data.index('Subtotal')):
         Costs.append(Data[j])
     j += 1
 
-# Dictionary to add as a new row
+# Dictionary to capture each datapoint
 new_receipt = {
     'Location': Location,
     'Date': TimeNDate,
@@ -63,7 +66,6 @@ new_receipt = {
 
 # Convert dictionary to DataFrame
 new_receipt_df = pd.DataFrame([new_receipt])
-
 
 # Display the updated DataFrame
 print(new_receipt_df)
